544eed92add7533a4801c677c6a1c938c3a11492
apple <NewUser@apples-MacBook-Pro.local>
2017-06-10 21:18:22 -0400

    Initial Commit

---
+++ Gamification2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gamification2
+//
+//  Created by New User on 6/10/17.
+//  Copyright © 2017 PENN STATE ABINGTON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

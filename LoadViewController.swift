f3c953a6a46e3a1b6c45d48979d30f0eee40d812
Win Ton <winton@Karis-Mac-mini.local>
2017-06-12 13:22:41 -0400

    win

--- LoadViewController.swift
+++ LoadViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class LoadViewController: UIViewController {
+class LoadViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
